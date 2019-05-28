1096f9c3f1f565bb3f2c3540a3dd69d20bd86a2a
darkroom51 <darkroom@wp.pl>
2019-05-28 23:13:38 +0200

    add form validation

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import '../css/main.scss';
 import { jump } from './jump';
+import { validateEmail, sendData } from './helpers';
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -45,6 +46,33 @@ document.addEventListener('DOMContentLoaded', function () {
   //signup form
   signupForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    //TODO validate form
+    var emailInput = document.querySelector('#email').value;
+    var gdprCheckbox = document.querySelector('#gdpr').checked;
+    var submitBtn = document.querySelector('#submit-btn');
+
+    if (!gdprCheckbox || !validateEmail(emailInput)){
+      renderMsg('error', 'Ups! Please check is your email OK and you checked the checkbox', submitBtn);
+      return;
+    }
+
+    var data = {email: emailInput};
+    renderMsg('info', 'Great you have been registered! We\'ll contact you soon', submitBtn);
+    document.getElementById('signup-form').reset();
+    sendData(data);
   })
+
+  function renderMsg(type, msg, button){
+    var msgElem = document.querySelector('.signup__msg');
+    msgElem.innerHTML = msg;
+    msgElem.classList.add('show');
+    msgElem.classList.add(type === 'error' ? 'show--error' : 'show--info');
+    button.disabled = true;
+    button.classList.add('btn--disabled');
+    setTimeout(function(){
+      msgElem.innerHTML = '';
+      msgElem.classList.remove('show', 'show--error', 'show--info');
+      button.disabled = false;
+      button.classList.remove('btn--disabled');
+    },6000);
+  }
 });
